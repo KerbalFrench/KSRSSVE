512df84bd06801aa99688ef65647e0d644d9476b
tony48 <antolinares01@gmail.com>
2019-06-16 18:58:59 -0400

    plgin

---
+++ src/KSRSSVE/CityLights.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using Utils;
+namespace KSRSSVE
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class CityLights : MonoBehaviour
+    {
+        GameObject cityLights;
+        MaterialPQS macro;
+        public void Start()
+        {
+            cityLights = GameObject.Find("EVE City Lights");
+            macro = cityLights.GetComponent<MaterialPQS>();
+        }
+
+        public void Update()
+        {
+            if (FlightGlobals.ActiveVessel.altitude < 10000 && macro.modEnabled)
+            {
+                macro.modEnabled = false;
+                macro.RebuildSphere();
+            }
+            else if (FlightGlobals.ActiveVessel.altitude >= 10000 && !macro.modEnabled)
+            {
+                macro.modEnabled = true;
+                macro.RebuildSphere();
+            }
+        }
+    }
+}
