da2d753253f4c469d9752d5f48b438e847bbd889
tony48 <antolinares01@gmail.com>
2020-03-15 12:07:20 -0400

    citylights fix

--- src/KSRSSVE/CityLights.cs
+++ src/KSRSSVE/CityLights.cs
@@ -1,31 +1,33 @@
 ﻿using System;
 using UnityEngine;
 using Utils;
+using CityLights;
+using System.Reflection;
+using EVEManager;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace KSRSSVE
 {
     [KSPAddon(KSPAddon.Startup.Flight, false)]
     public class CityLights : MonoBehaviour
     {
-        GameObject cityLights;
-        MaterialPQS macro;
+        //CityLightsObject cityLights;
+        //MaterialPQS macro;
         public void Start()
         {
-            cityLights = GameObject.Find("EVE City Lights");
-            macro = cityLights.GetComponent<MaterialPQS>();
-        }
+            GlobalEVEManager manager = FindObjectOfType<GlobalEVEManager>();
+            List<EVEManagerBase> managers = (List < EVEManagerBase > )manager.GetType().GetFields(BindingFlags.Static | BindingFlags.NonPublic).First(f => f.Name == "Managers").GetValue(manager);
+            EVEManagerBase cityLightsManager = managers.First(m => m.configName == "EVE_CITY_LIGHTS");
+            cityLightsManager.GetType().GetMethods(BindingFlags.Instance | BindingFlags.Public).First(m => m.Name == "Apply").Invoke(cityLightsManager, null);
+            //List<IEVEObject> list = (List < IEVEObject > )cityLightsManager.GetType().GetField("ObjectList").GetValue(cityLightsManager);
 
-        public void Update()
-        {
-            if (FlightGlobals.ActiveVessel.altitude < 10000 && macro.modEnabled)
-            {
-                macro.modEnabled = false;
-                macro.RebuildSphere();
-            }
-            else if (FlightGlobals.ActiveVessel.altitude >= 10000 && !macro.modEnabled)
-            {
-                macro.modEnabled = true;
-                macro.RebuildSphere();
-            }
+            //typeof(CityLightsManager).GetMethod("ApplyConfigNode", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(inst, null);
+            //CelestialBody b = FlightGlobals.Bodies.Find(x => x.isHomeWorld == true);
+            //Transform t = Tools.GetScaledTransform("Earth");
+            //ScaledCityComponent scaledCity = t.gameObject.GetComponent<ScaledCityComponent>();
+            //typeof(ScaledCityComponent).GetMethod("Apply", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(scaledCity, new UnityEngine.Object[] {CityLightsObject. });
+
         }
     }
 }
