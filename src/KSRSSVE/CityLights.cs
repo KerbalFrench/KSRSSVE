c97d6e5c2fb630e094916f2ff198008924cbe6f6
tony48 <antolinares01@gmail.com>
2020-03-16 14:35:03 -0400

    Update CityLights.cs

--- src/KSRSSVE/CityLights.cs
+++ src/KSRSSVE/CityLights.cs
@@ -20,14 +20,6 @@ namespace KSRSSVE
             List<EVEManagerBase> managers = (List < EVEManagerBase > )manager.GetType().GetFields(BindingFlags.Static | BindingFlags.NonPublic).First(f => f.Name == "Managers").GetValue(manager);
             EVEManagerBase cityLightsManager = managers.First(m => m.configName == "EVE_CITY_LIGHTS");
             cityLightsManager.GetType().GetMethods(BindingFlags.Instance | BindingFlags.Public).First(m => m.Name == "Apply").Invoke(cityLightsManager, null);
-            //List<IEVEObject> list = (List < IEVEObject > )cityLightsManager.GetType().GetField("ObjectList").GetValue(cityLightsManager);
-
-            //typeof(CityLightsManager).GetMethod("ApplyConfigNode", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(inst, null);
-            //CelestialBody b = FlightGlobals.Bodies.Find(x => x.isHomeWorld == true);
-            //Transform t = Tools.GetScaledTransform("Earth");
-            //ScaledCityComponent scaledCity = t.gameObject.GetComponent<ScaledCityComponent>();
-            //typeof(ScaledCityComponent).GetMethod("Apply", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(scaledCity, new UnityEngine.Object[] {CityLightsObject. });
-
         }
     }
 }
